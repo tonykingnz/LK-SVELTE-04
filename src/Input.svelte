6b2e734e8a499e56fbebff8be45ee60773559ea4
Jose Antonio Maria de Rezende Silva <30808376+tonykingnz@users.noreply.github.com>
2020-12-18 15:12:13 -0300

    Update Input.svelte

--- src/Input.svelte
+++ src/Input.svelte
@@ -1,65 +1,82 @@
 <script>
-    export let placeholder = "Write here";
-    	
-    	let autocompletion = [];
-    	export let inputText = "";
-    	export let arrayToSearch = [];
-    	
-        setInterval(function() {
-            if (inputText != undefined && inputText != "") {
-                autocompletion = [];
+	export let placeholder = "Write here";
 
-                arrayToSearch.forEach(checkAndAdd);
-                function checkAndAdd(item) {
-                    if (item.toUpperCase().includes(inputText.toUpperCase())) {
-                        autocompletion = [...autocompletion, item];
-                    }
-                }
-            } else {
-                autocompletion = [];
-            }
-        }, 500);
-        
-        let i = 0;
-    	let inputTextCandidate = autocompletion[0];
-        
-        function handleKey() {
-    	    if (autocompletion.length > 0) {
-                if (event.key == "ArrowUp" && inputTextCandidate != autocompletion[0])  {
-    		        inputTextCandidate = autocompletion[i];
-    			    i -= 1;
-                }
-    		    else if (event.key == "ArrowDown" && inputTextCandidate != autocompletion[autocompletion.length - 1]) {
-                    inputTextCandidate = autocompletion[i];
-    			    i += 1;
-                }
-                else if (event.keyCode == 13){
-                    if (inputTextCandidate != undefined && inputTextCandidate != ""){
-                        inputText = inputTextCandidate;
-                    }
-                }
-    		    return handleKey;
-            }
-        }
+	let autocompletion = [];
+	export let inputText = "";
+	export let arrayToSearch = [];
+	
+	$: hidden = inputText ? false:true;
+	
+	let timer;
+	function debounce (v) {
+		hidden = false;
+		clearTimeout(timer);
+		timer = setTimeout(() => {
+			if (v != undefined && v != "") {
+				autocompletion = [];
+				arrayToSearch.forEach(checkAndAdd);
+				function checkAndAdd(item) {
+					if (item.toUpperCase().includes(v.toUpperCase())) {
+						autocompletion = [...autocompletion, item];
+						
+					}
+				}
+			} else {
+				autocompletion = [];
+			}
+		}, 500);
+	}
+	
+	let i = 0;
+	let inputTextCandidate = autocompletion[0];
+	function handleKey() {
+		/*console.log(event.key);*/
+		
+		if (autocompletion.length > 0) {
+			if (event.key == "ArrowUp" && inputTextCandidate != autocompletion[0])  {
+				inputTextCandidate = autocompletion[i];
+				i -= 1;
+			}
+			else if (event.key == "ArrowDown" && inputTextCandidate != autocompletion[autocompletion.length - 1]) {
+				inputTextCandidate = autocompletion[i];
+				i += 1;
+			}
+			else if (event.key == "Enter"){
+				if (inputTextCandidate != undefined && inputTextCandidate != ""){
+					inputText = inputTextCandidate;
+				}
+			}
+			else if (event.key == "Escape"){
+				hidden = true
+			}
+			return handleKey;
+		}
+	}
+	$: console.log(hidden);
+	let attenction = "NotClicked";
+	
+	const clicked = event => filteredItems = [event];
+	
+	function clickedOut(event) {
+		hidden = true
+	}
 </script>
 
-<body>
+<main on:click|stopPropagation={clickedOut}>
     <div class="container">
-        <input type="text" bind:value={inputText} {placeholder}>
-        <div class="sugestion">
+
+        <input type="text" bind:value={inputText} {placeholder} on:input={({ target: {value} }) => debounce(value)} />
+
+        <div class="sugestion" class:sugestion-hidden="{hidden}">
             {#if inputText.length > 0 && autocompletion.length < 1}
                  <ul>
                      <p>Not found</p>
                 </ul>
             {:else if inputText != "" && autocompletion != []}
                 <ul>
-                    {#if inputTextCandidate != undefined}
-                        <p>Press enter to select: {inputTextCandidate}</p>
-                    {:else}
-                        <p>Use the arrows keys to find in the sugestions.</p>
-                    {/if}
+										<br>
                     {#each autocompletion as match, i}
-                        <li on:click="{() => inputText = match}">{match}</li>
+                        <li class:selected="{match == inputTextCandidate}" on:click="{() => inputText = match}">{match}</li>
                     {/each}
                 </ul>
             {:else}
@@ -68,37 +85,59 @@
         </div> 
     </div>
     <br>
-</body>
+</main>
 
-<svelte:window on:keydown={handleKey} />
+<svelte:window on:keydown={handleKey}/>
 
 <style>
-    ul {
+	main {
+		padding: 0;
+		margin: 0;
+	}
+	ul {
 		background-color: #eaeaea;
-        border-color: #a5a5a5;
-    	border: 2.5px;
+		border-color: #a5a5a5;
+		border: 2.5px;
 		border-style: solid;
-        border-radius: 10px;
-    	width: 180px;
-        height: 120px;
-        overflow-x: hidden;
-        overflow-y: auto;
-        text-align: left;
-    }
-    p {
-        text-align: left;
-        border: none;
-        color: #567987;
-    }
-    li {
-        cursor: pointer;
-    }
-    	.container{
-      	position: relative;
-      }
-      .sugestion{
-        top: 18px;
-    	z-index : 99;
-      	position: absolute;
-       }
+		border-radius: 10px;
+		width: 180px;
+		height: 120px;
+		overflow-x: hidden;
+		overflow-y: auto;
+		text-align: left;
+		list-style-type: none;
+		
+	}
+	
+	li {
+		background-color: #cccccc;
+		cursor: pointer;
+		margin: 0;
+		position: relative;
+		left: -30px;
+	}
+	
+	.selected {
+		background-color: #aaaaaa;
+		cursor: pointer;
+	}
+	
+	p {
+		text-align: left;
+		border: none;
+		color: #567987;
+	}
+	
+	.container{
+		position: relative;
+	}
+	.sugestion{
+		top: 18px;
+		z-index : 99;
+		position: absolute;
+	}
+	.sugestion-hidden {
+		display: none;
+	}
+
 </style>
